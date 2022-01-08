d05a921d692b2d1f780b5eb1349034fe73f30c9f
kjcaway <whdckstngh@naver.com>
2022-01-08 13:41:11 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Springboot WebMVC Sample Project
+
+---------------------
+
+## env
+- java 11
+- intellij ultimate
+- windows 10
+- springboot 2.5.8
+- gradle 7.3
+
+
---
+++ src/test/java/me/javaexample/javademo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.javaexample.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
