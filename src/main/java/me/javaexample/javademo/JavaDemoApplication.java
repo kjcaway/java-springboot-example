b807e439ffcfdb7def4f5fb0cb102fa90ed96144
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2022-08-21 22:30:12 +0900

    **MOD -me.anno package scan

--- src/main/java/me/javaexample/javademo/JavaDemoApplication.java
+++ src/main/java/me/javaexample/javademo/JavaDemoApplication.java
@@ -2,8 +2,10 @@ package me.javaexample.javademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.javaexample.javademo", "me.anno"})
 public class JavaDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/me/javaexample/javademo/config/AopConfig.java
@@ -0,0 +1,13 @@
+package me.javaexample.javademo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+import org.springframework.context.annotation.LoadTimeWeavingConfigurer;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.instrument.classloading.ReflectiveLoadTimeWeaver;
+
+//@Configuration
+//@EnableLoadTimeWeaving
+//public class AopConfig {
+//
+//}
