a53ff4be5c87b68644b8b91f41dd2d06614a6a46
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2023-01-07 14:32:52 +0900

    **ADD -sampling popup and notice

--- src/main/resources/views/main/index.js
+++ src/main/resources/views/main/index.js
@@ -1,4 +1,11 @@
 ﻿let MAIN = {
+    initialize: function() {
+        if(MAIN.isPopup()){
+            if(!$.cookie("notice")){
+                $('#noticeModal').modal('show');
+            }
+        }
+    },
     onAddCardClick: function (e) {
         const jqCard = $(`
             <div class="col-sm-3 mt-2">
@@ -17,12 +24,19 @@
         jqCard.hide();
         $("#cardGroupDiv").append(jqCard);
         jqCard.show("normal");
+    },
+    isPopup: function() {
+        const todayDate = new Date();
+        const dueDate = new Date('2023-01-10T05:00:00.000Z');
+        return todayDate.getTime() < dueDate.getTime();
     }
 };
 
 
 
 $(document).ready(function () {
+    MAIN.initialize();
+
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
@@ -30,6 +44,18 @@ $(document).ready(function () {
     });
 
     $("#addCardBtn").on("click", MAIN.onAddCardClick);
+
+    $('#noticeModal').on('hidden.bs.modal', function (e) {
+        const todayCloseCheck = $("#noticeTodayCloseChk").is(":checked");
+
+        if(todayCloseCheck){
+            const noticeCookie = $.cookie("notice");
+            if(!noticeCookie){
+                $.cookie("notice", true, { expires: 1, path: '/' });
+            }
+        }
+    })
+
     console.log(CURRENT_ENV);
     console.log(OTHER_SERVICE_URL);
 });
