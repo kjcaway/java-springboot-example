0eaca50530b1a36c4228719a8362aeab7a19efc5
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2023-03-05 16:48:23 +0900

    **ADD -add sample test code using mockmvc -get or post request cases

--- src/test/java/me/javaexample/javademo/api/member/MemberControllerTest.java
+++ src/test/java/me/javaexample/javademo/api/member/MemberControllerTest.java
@@ -1,24 +1,56 @@
 package me.javaexample.javademo.api.member;
 
 import me.javaexample.javademo.api.IntegrationTest;
-import me.javaexample.javademo.api.member.dto.MemberDto;
-import me.javaexample.javademo.api.member.service.MemberService;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class MemberControllerTest extends IntegrationTest {
 
     @Autowired
-    MemberService memberService;
+    private MockMvc mockMvc;
 
     @Test
-    void getMembers() {
-        System.out.println("StartTest!");
-        List<MemberDto> mList = memberService.getMembers();
+    void _01_멤버조회() throws Exception {
 
-        Assertions.assertEquals(mList.size(), 2);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/member"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].name", is("kang")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].name", is("kim")));
+    }
+
+    @Test
+    void _02_멤버추가() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/member")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"alice\", \"email\":\"alice@gmail.com\", \"category\":\"M001\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/member"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data", hasSize(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].name", is("kang")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].name", is("kim")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[2].name", is("alice")));
     }
 }
